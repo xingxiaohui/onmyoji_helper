dd812e3f978e2a74100ac5b917910a5a56312789
xingxiaohui <1559237979@qq.com>
2019-12-26 21:15:17 +0800

    新增拒绝封印，御魂自动组队

--- onmyoji_helper/config.py
+++ onmyoji_helper/config.py
@@ -3,13 +3,15 @@
 """
 
 # 觉醒图片名称
-kylin_image_names = ['tiaozhan.png', 'jieshu.png', 'jiangli.png', 'jixu.png']
+kylin_image_names = ['tiaozhan.png', 'jieshu.png', 'jiangli.png', 'jixu.png', 'jujue.png']
 
 # 御魂图片名称
-orochi_image_names = ['tiaozhan.png', 'tiaozhan1.png', 'jieshu.png', 'jiangli.png', 'jixu.png']
+orochi_image_names = ['tiaozhan.png', 'tiaozhan1.png', 'jieshu.png', 'jiangli.png', 'jixu.png', 'jujue.png',
+                      'tiaozhan2.png', 'jieshou.png', 'jieshou1.png', 'moren.png', 'queding.png']
 
 # 组队战斗图片名称
-troops_image_names = ['zudui.png', 'pipei.png', 'zhunbei.png', 'jieshu.png', 'jiangli.png', 'jixu.png']
+troops_image_names = ['zudui.png', 'pipei.png', 'zhunbei.png', 'jieshu.png', 'jiangli.png', 'jixu.png', 'jujue.png']
+
 # 探索图片名称
 explore_image_names = ['rukou.png', 'tansuo.png', 'tiaozhan.png', 'tiaozhan2.png', 'jieshu.png', 'jiangli.png',
                        'jiangli1.png', 'jiangli2.png', 'jixu.png']
